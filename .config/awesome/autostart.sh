a8bf02c4bd568aad59dce865a266d875b258c6d2
streamsniperrty <54614860+streamsniperrty@users.noreply.github.com>
2020-12-20 20:48:50 -0500

    Create autostart.sh

---
+++ .config/awesome/autostart.sh
@@ -0,0 +1,12 @@
+# Sets dual monitors, change HDMI1 to your according output
+# xrandr --output eDP-1 --auto --output HDMI-1 --left-of eDP-1 &
+
+# Set second monitor as primary monitor, change HDMI1 to your according output
+# xrandr --output eDP1 --off --output HDMI1 --mode 1600x900 &
+
+feh --bg-scale /home/labibmahmud/.config/wallpapers/BeautifulScenery.png &
+compton -f &
+$HOME/.config/polybar/launch.sh &
+xbindkeys &
+exec /usr/lib/x86_64-linux-gnu/xfce4/notifyd/xfce4-notifyd &
+~                                                               
